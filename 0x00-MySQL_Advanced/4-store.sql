55b3f0fad776fbf37866a77e205756499991561b
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-26 18:39:54 +0000

    5-valid_email.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,15 @@
 -- Update table based on value in anothe table
 
-UPDATE items
-JOIN orders ON items.name = orders.item_name
-SET items.quantity = items.quantity - orders.number
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+
+DELIMITER ;
