06a3061ddcc6f685feecdc4cb563a51358f3bca4
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-27 17:40:46 +0000

    Mandatory

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates an index idx_name_first
+-- on the table names and the first letter of name.
+
+-- CREATE INDEX idx_name_first ON names (name(255), LEFT(name, 1));
+
+CREATE INDEX idx_name_first ON names;
