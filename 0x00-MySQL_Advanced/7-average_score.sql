6d0e2dfe8aef7b73c1ec71d250796c6ac376fb9b
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-27 18:27:05 +0000

    Attempt fix on 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -13,12 +13,11 @@ BEGIN
     -- Compute the average score
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id
+    GROUP BY corrections.user_id;
 
-    -- Update the user's average score
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    UPDATE users SET average_score = avg_score WHERE users.id = user_id;
+
 END;
 //
 
